d3399f6f36da8d8439d76b00c4cf7ab73d07b55e
Mukund <mukunddubey10000@gmail.com>
2025-01-01 20:28:31 +0530

    UI, redux-toolkit setup done

---
+++ src/components/Menu/index.js
@@ -0,0 +1,39 @@
+import { useDispatch, useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEraser, faFileArrowDown, faPencil, faRotateLeft, faRotateRight } from "@fortawesome/free-solid-svg-icons";
+import cx from 'classnames';
+import styles from "./index.module.css";
+
+import { MENU_ITEMS } from "../constants";
+import { menuItemClick, actionItemClick } from "@/slice/menuSlice";
+
+const Menu = () => {
+    const dispatch = useDispatch();
+    const activeMenuItem = useSelector(state => state.menu.activeMenuItem);
+
+    const handleMenuClick = (itemName) => {
+        dispatch(menuItemClick(itemName))
+    };
+
+    return (
+        <div className={styles.menuContainer}>
+            <div className={cx(styles.iconWrapper, { [styles.active]: activeMenuItem === MENU_ITEMS.PENCIL })} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}>
+                <FontAwesomeIcon icon={faPencil} className={styles.icon} />
+            </div>
+            <div className={cx(styles.iconWrapper, { [styles.active]: activeMenuItem === MENU_ITEMS.ERASER })} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}>
+                <FontAwesomeIcon icon={faEraser} className={styles.icon} />
+            </div>
+            <div className={cx(styles.iconWrapper, { [styles.active]: activeMenuItem === MENU_ITEMS.REDO })} onClick={() => handleMenuClick(MENU_ITEMS.REDO)} >
+                <FontAwesomeIcon icon={faRotateRight} className={styles.icon} />
+            </div>
+            <div className={cx(styles.iconWrapper, { [styles.active]: activeMenuItem === MENU_ITEMS.UNDO })} onClick={() => handleMenuClick(MENU_ITEMS.UNDO)} >
+                <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
+            </div>
+            <div className={cx(styles.iconWrapper, { [styles.active]: activeMenuItem === MENU_ITEMS.DOWNLOAD })} onClick={() => handleMenuClick(MENU_ITEMS.DOWNLOAD)} >
+                <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon} />
+            </div>
+        </div>
+    )
+}
+
+export default Menu
